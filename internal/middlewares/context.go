2592d103167ff5ace0bc141a2b5334b70d87fb3d
Terry Zhang <tz624@nyu.edu>
2020-04-11 20:15:54 +0800

    debug

--- internal/middlewares/context.go
+++ internal/middlewares/context.go
@@ -9,10 +9,8 @@ import (
 // WrapContext put database and request in r.Context
 func WrapContext(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		ctx, err := context.WithTimeout(req.Context(), 10*time.Second)
-		if err != nil {
-			return
-		}
+		ctx, cancel := context.WithTimeout(req.Context(), 10*time.Second)
+		defer cancel()
 		h(w, req.WithContext(ctx))
 	}
 }
