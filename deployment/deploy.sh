5c70030f418f5d88cf9923d155135b933097855c
Terry Zhang <tz624@nyu.edu>
2020-06-28 20:37:25 +0800

    https support

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,5 +4,6 @@
 # 1. Docker installed on remote machine
 # 2. Docker Compose installed on remote machine
 
+docker-compose -f nginx-proxy-compose.yaml up -d || exit
 docker-compose -f service-compose.yaml build --build-arg app_env=production || exit
 docker-compose -f service-compose.yaml up -d || exit
